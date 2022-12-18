b39f98d4c6f05140dff3532fe37aae8ef41b059a
invalnerebl <36478860+invalnerebl@users.noreply.github.com>
2022-12-18 17:59:25 +0100

    feat: add more complicated scalar function

--- Functions.sql
+++ Functions.sql
@@ -19,3 +19,19 @@ FROM Products
 DECLARE @myValue MONEY
 EXEC @myValue = dbo.UnitPricePlusOne @Amount = 1
 SELECT @myValue
+GO
+
+CREATE FUNCTION NumberOfOrders(@EmployeeNumber INT)
+RETURNS INT
+AS 
+BEGIN
+	DECLARE @NumberOfOrders INT
+	SELECT @NumberOfOrders = COUNT(*)
+	FROM Orders
+	WHERE EmployeeID = @EmployeeNumber
+	RETURN @NumberOfOrders
+END
+GO
+
+SELECT DISTINCT EmployeeID, dbo.NumberOfOrders(EmployeeID) AS OrdNum
+FROM Orders
